de35524ee2875bb85a16756eb063455c6191df0a
nacika <takahashi.takehiro@bulbit.jp>
2019-01-08 19:43:14 +0900

    feat: add requested_reviewers macro

--- src/reviewkun.ts
+++ src/reviewkun.ts
@@ -4,10 +4,12 @@ declare var HtmlService: any;
 declare var UrlFetchApp: any;
 declare var Logger: any;
 
-var property      = PropertiesService.getScriptProperties()
-var SLACK_API_URL = property.getProperty("SLACK_API_URL")
-var spreadsheet   = SpreadsheetApp.openById(property.getProperty("spreadsheet_id"))
-var sheet         = spreadsheet.getSheetByName("設定")
+var property = PropertiesService.getScriptProperties();
+var SLACK_API_URL = property.getProperty("SLACK_API_URL");
+var spreadsheet = SpreadsheetApp.openById(
+    property.getProperty("spreadsheet_id")
+);
+var sheet = spreadsheet.getSheetByName("設定");
 
 namespace web {
     export function doPost(e) {
@@ -21,87 +23,111 @@ namespace web {
                 return;
             }
 
-            postToSlack(repoName, content, config)
-        })
+            postToSlack(repoName, content, config);
+        });
 
-        return HtmlService.createHtmlOutput(JSON.stringify({ repoName, action, content, configs }));
+        return HtmlService.createHtmlOutput(
+            JSON.stringify({ repoName, action, content, configs })
+        );
     }
 
     // payload と spreadsheet から必要な情報を集める
     function fromPayload(payload) {
         var repoName = payload.repository.full_name;
-        var repoNames = sheet.getRange("A2:A")
+        var repoNames = sheet.getRange("A2:A");
 
         var configs = [];
         for (var i = 1; i <= repoNames.getHeight(); i++) {
-            var cell = repoNames.getCell(i, 1)
+            var cell = repoNames.getCell(i, 1);
             if (repoName === cell.getValue()) {
-                var rowIndex = cell.getRowIndex()
-                var configRow = sheet.getRange(rowIndex, 1, 1, 10).getValues()[0]
+                var rowIndex = cell.getRowIndex();
+                var configRow = sheet
+                    .getRange(rowIndex, 1, 1, 10)
+                    .getValues()[0];
                 configs.push({
                     channel: configRow[1],
                     labelCondition: configRow[2],
-                    botName: configRow[3] || 'reviewkun',
-                    botIcon: configRow[4] || ':eyes:',
+                    botName: configRow[3] || "reviewkun",
+                    botIcon: configRow[4] || ":eyes:",
                     showBody: configRow[5],
-                    message: configRow[6],
-                })
+                    message: configRow[6]
+                });
             }
         }
 
         return {
             repoName: repoName,
-            action  : payload.action,
+            action: payload.action,
             content: {
-                label : payload.label,
+                label: payload.label,
                 target: payload.issue || payload.pull_request,
-                user  : payload.sender,
+                user: payload.sender
             },
-            configs: configs,
+            configs: configs
         };
     }
 
     function postToSlack(repoName, content, config) {
-        var label   = '`' + content.label.name + '`'
-        var user    = content.user.login.replace(/^(.)/, "$1/");
-        var title   = '<' + content.target.html_url + '|' + content.target.title + '>'
-        var botName = '<' + spreadsheet.getUrl() + '|ReviewKun>'
-        var message = config.message.replace(/{{assignee}}/g, (content.target.assignees[0] || {}).login);
-        var text = label + ' (by ' + user + '): ' + title + ' ' + message + ' from ' + botName;
+        var label = "`" + content.label.name + "`";
+        var user = content.user.login.replace(/^(.)/, "$1/");
+        var title =
+            "<" + content.target.html_url + "|" + content.target.title + ">";
+        var botName = "<" + spreadsheet.getUrl() + "|ReviewKun>";
+        var message = config.message.replace(
+            /{{assignee}}/g,
+            (content.target.assignees[0] || {}).login
+        ).replace(
+            /{{requested_reviewers}}/g,
+            content.target.requested_reviewers.map((v) => v.login).join(" ")
+        );
+        var text =
+            label +
+            " (by " +
+            user +
+            "): " +
+            title +
+            "\n" +
+            message +
+            " from " +
+            botName;
 
-        var body    = config.showBody ? content.target.body : '';
-        var color   = content.label.color;
+        var body = config.showBody ? content.target.body : "";
+        var color = content.label.color;
 
         var res = UrlFetchApp.fetch(SLACK_API_URL, {
-            method: 'post',
+            method: "post",
             payload: {
                 payload: JSON.stringify({
-                    username   : config.botName,
+                    username: config.botName,
+                    link_names : true,
                     // text       : text,
-                    icon_emoji : config.botIcon,
-                    channel    : config.channel,
+                    icon_emoji: config.botIcon,
+                    channel: config.channel,
                     attachments: [
                         {
-                            text: [ text, body ].join("\n"),
+                            text: [text, body].join("\n"),
                             color: "#" + color,
-                            mrkdwn_in: ["text"],
+                            mrkdwn_in: ["text"]
                         }
                     ],
-                    link_names: 1
-                }),
+                })
             }
         });
-        Logger.log(res.getContent())
+        Logger.log(res.getContent());
     }
 
     function needToPost(action, content, config) {
-        if (action !== 'labeled') {
+        if (action !== "labeled") {
             return false;
         }
         if (!config) {
             return false;
         }
-        if (!content.label.name.match(new RegExp(config.labelCondition || 'レビュー'))) {
+        if (
+            !content.label.name.match(
+                new RegExp(config.labelCondition || "レビュー")
+            )
+        ) {
             return false;
         }
         return true;
